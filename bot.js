2d1957c1c4efb61e54388e6c67d447a7254776a3
DylanJansen <40144787+DylanJansen@users.noreply.github.com>
2018-06-18 17:54:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,13 @@
-/*
- * Discord Bot Builder Bot
- * Version 1.2.0
- * Robert Borghese
- */
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log('I am ready!');
+});
 
 const Files = require(require('path').join(__dirname, 'js', 'Main.js')).Files;
 
-if(!process.send) {
-
-Files.initStandalone();
-
-} else {
-
-process.on('message', function(content) {
-	Files.initBotTest(content);
-    }
-
 });
-    
+
 // THIS  MUST  BE  THIS  WAY
 client.login(process.env.BOT_TOKEN);
-
-
